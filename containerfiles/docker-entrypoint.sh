7b92cba47341c785f468caa8717ca0c06f87ecdd
nikelmark <nikelmark@gmail.com>
2017-07-03 15:02:07 +0200

    Update docker-entrypoint.sh

--- containerfiles/docker-entrypoint.sh
+++ containerfiles/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 
-envsubst < /opt/app-root/etc/conf.d/50-mpm-tuning.conf.template > /opt/app-root/etc/conf.d/50-mpm-tuning.conf
-envsubst < /opt/app-root/etc/conf.d/00-documentroot.conf.template > /opt/app-root/etc/conf.d/00-documentroot.conf
-
 exec httpd -D FOREGROUND
